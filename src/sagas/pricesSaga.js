8520de67bf267bc852ea2a5cc3929a4495dbdb14
galinamorozova <sidh_g@mail.ru>
2023-03-31 22:52:38 +0300

    Update pricesSaga.js

--- src/sagas/pricesSaga.js
+++ src/sagas/pricesSaga.js
@@ -7,13 +7,13 @@ function* pricesWorker() {
     let locationData = yield select(location);
     let checkInData = yield select(checkIn);
     let checkOutData = yield select(checkOut);
-    const data = yield call(fetch, 'https://engine.hotellook.com/api/v2/cache.json:splat?',
+    const data = yield call(fetch, 'https://engine.hotellook.com/api/v2/cache.json:splat?' +'?location=' + `${locationData}`
+        + '&currency=rub&' + 'checkIn=' + `${checkInData}` + '&checkOut=' + `${checkOutData}` + '&limit=5',
         { method: 'GET',
-            mode: 'cors',
-            body: 'location=' + locationData + 'currency=rub' + 'checkIn=' + {checkInData} + 'checkOut=' + checkOutData,
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json'
-        }});
+        });
     const formattedData = yield data.json();
     yield put(getPricesSuccess(formattedData));
 }
