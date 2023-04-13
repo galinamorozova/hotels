ccc1d6fa014555c0916d96b6d4e50dde18356e8a
galinamorozova <sidh_g@mail.ru>
2023-04-13 15:36:51 +0300

    Update pricesSaga.js

--- src/sagas/pricesSaga.js
+++ src/sagas/pricesSaga.js
@@ -7,7 +7,7 @@ function* pricesWorker() {
     let locationData = yield select(location);
     let checkInData = yield select(checkIn);
     let checkOutData = yield select(checkOut);
-    const data = yield call(fetch, 'http://engine.hotellook.com/api/v2/cache.json?location=Moscow&currency=rub&checkIn=2023-12-10&checkOut=2023-12-12&limit=10', { method: 'GET', referer: 'https://main--startling-druid-b9d701.netlify.app/hotels'});
+    const data = yield call(fetch, 'https://engine.hotellook.com/api/v2/cache.json?location=Moscow&currency=rub&checkIn=2023-12-10&checkOut=2023-12-12&limit=10', { method: 'GET', referer: 'https://main--startling-druid-b9d701.netlify.app/hotels'});
 
     // const data = yield call(fetch, 'http://engine.hotellook.com/api/v2/cache.json:splat?' +'location=' + `${locationData}`
     //     + '&currency=rub&' + 'checkIn=' + `${checkInData}` + '&checkOut=' + `${checkOutData}` + '&limit=5', { method: 'GET'});
